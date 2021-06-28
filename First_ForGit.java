8502f7d76a5d4e830760f4de254a24ad28a71cdd
DenisBaturo <47177495+DenisBaturo@users.noreply.github.com>
2021-06-28 09:02:51 +0300

    Update First_ForGit.java

--- First_ForGit.java
+++ First_ForGit.java
@@ -1,23 +1,25 @@
 import java.util.*;
 
-public class Level1 {
+public class Level1_2 {
 
     public static int odometer(int [] oksana) {
 
-        int t = 0;   // Общее количество времени
-        int v = 0;   // Общее количество скорости
+        int t = 0;      // Количество времени
+        int v = 0;      // Количество скорости
+        int t_pre = 0;  // Для сохранения предыдущего значения времени.
+        int s = 0;      // Дистанция.
 
         for(int i = 0; i < oksana.length; i++){
-
+            
             if((i % 2) == 0){
-                v += oksana[i];      // Узнать общее количество скорости
-            }
-            else {
-                t += oksana[i];     // Узнать общее количество времени
+                v = oksana[i];     
+                t_pre = t;          // Запоминаем предыдущее значение времени.
+                t = oksana[i+1];    
+                s += v * (t - t_pre);
             }
         }
 
         // Общая формула для вычисления скорости.
-        return v * t;
+        return s;
     }
 }
